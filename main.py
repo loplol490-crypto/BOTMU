70ec1494549c5bb631409da1f58d6120a008abd5
loplol490-crypto <loplol490@gmail.com>
2026-07-02 17:27:54 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-BOT_TOKEN = "8511583821:AAGkuPw8bSj9PnAnZHaZJUCDsSOLsTw7iF0"
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Бот работает!")
 
-app = ApplicationBuilder().token(BOT_TOKEN).build()
-app.add_handler(CommandHandler("start", start))
+def main():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
 
-app.run_polling()
+if __name__ == "__main__":
+    main()
