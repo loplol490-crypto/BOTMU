28b53b0bfd19087828749473720ea3839fefe551
loplol490-crypto <loplol490@gmail.com>
2026-07-04 19:06:52 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
 from config import BOT_TOKEN
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    CallbackQueryHandler,
+    ContextTypes,
+)
+
 from handlers.start import start
+
+
+# /start
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Выбери язык 👇")
+
+
+# обработка кнопок
+async def handle_language(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+
+    data = query.data
+
+    if data == "lang_ru":
+        await query.edit_message_text("Ты выбрал 🇷🇺 Русский")
+    elif data == "lang_en":
+        await query.edit_message_text("You selected 🇬🇧 English")
+    elif data == "lang_kg":
+        await query.edit_message_text("Сиз 🇰🇬 Кыргыз тилин тандадыңыз")
 
 
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(CallbackQueryHandler(handle_language))
 
     app.run_polling()
 
