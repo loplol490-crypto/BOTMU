f8d195829cdea33e12128001897e838379dfe6c6
loplol490-crypto <loplol490@gmail.com>
2026-07-04 18:59:00 +0600

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,6 +1,10 @@
 from telegram import Update
 from telegram.ext import ContextTypes
+from keyboards.language_keyboard import language_keyboard
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Бот работает!")
+    await update.message.reply_text(
+        "Выбери язык 👇",
+        reply_markup=language_keyboard()
+    )
