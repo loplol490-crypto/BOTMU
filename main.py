fc6b00466a70e4abea8f55e72684181e09b46743
loplol490-crypto <loplol490@gmail.com>
2026-07-04 18:36:26 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-import os
+from config import BOT_TOKEN
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-if not BOT_TOKEN:
-    raise Exception("BOT_TOKEN НЕ НАЙДЕН в Environment Variables")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Бот работает!")
+
 
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
+
 
 if __name__ == "__main__":
     main()
