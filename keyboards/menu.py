8c3045fffd691f8eac1bc749cd043dd247a75396
loplol490-crypto <loplol490@gmail.com>
2026-07-04 19:35:38 +0600

    Update menu.py

--- keyboards/menu.py
+++ keyboards/menu.py
@@ -1,1 +1,12 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
+
+def main_menu():
+    keyboard = [
+        [InlineKeyboardButton("🗺 Гид по Кыргызстану", callback_data="guide")],
+        [InlineKeyboardButton("🍜 Еда", callback_data="food")],
+        [InlineKeyboardButton("🚌 Транспорт", callback_data="transport")],
+        [InlineKeyboardButton("⚙️ Настройки", callback_data="settings")],
+    ]
+
+    return InlineKeyboardMarkup(keyboard)
