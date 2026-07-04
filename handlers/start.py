f4598150486948de1283e4ccb365fd51f58d4b08
loplol490-crypto <loplol490@gmail.com>
2026-07-04 19:37:14 +0600

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,22 +1,23 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 from keyboards.language_keyboard import language_keyboard
+from keyboards.menu import main_menu
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     lang = context.user_data.get("lang")
 
-    if lang == "ru":
-        text = "Привет 👋 Ты уже выбрал русский язык"
-    elif lang == "en":
-        text = "Hello 👋 You already selected English"
-    elif lang == "kg":
-        text = "Салам 👋 Сен кыргыз тилин тандагансың"
-    else:
-        text = "Выбери язык 👇"
+    # если язык не выбран
+    if not lang:
+        await update.message.reply_text(
+            "Выбери язык 👇",
+            reply_markup=language_keyboard()
+        )
+        return
 
+    # если язык уже есть → показываем меню
     await update.message.reply_text(
-        text,
-        reply_markup=language_keyboard()
+        "Главное меню 👇",
+        reply_markup=main_menu()
     )
