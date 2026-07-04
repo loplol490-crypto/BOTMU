7a1e342646c478a1b8c0c330c454351f589f9fb2
loplol490-crypto <loplol490@gmail.com>
2026-07-04 19:27:45 +0600

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -4,7 +4,19 @@ from keyboards.language_keyboard import language_keyboard
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    lang = context.user_data.get("lang")
+
+    if lang == "ru":
+        text = "Привет 👋 Ты уже выбрал русский язык"
+    elif lang == "en":
+        text = "Hello 👋 You already selected English"
+    elif lang == "kg":
+        text = "Салам 👋 Сен кыргыз тилин тандагансың"
+    else:
+        text = "Выбери язык 👇"
+
     await update.message.reply_text(
-        "Выбери язык 👇",
+        text,
         reply_markup=language_keyboard()
     )
