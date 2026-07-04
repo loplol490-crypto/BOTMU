c96699e8f61ecbc89a5364effcc4dd09e8d99714
loplol490-crypto <loplol490@gmail.com>
2026-07-04 18:57:15 +0600

    Update language_keyboard.py

--- keyboards/language_keyboard.py
+++ keyboards/language_keyboard.py
@@ -1,1 +1,15 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
+
+def language_keyboard():
+    keyboard = [
+        [
+            InlineKeyboardButton("🇷🇺 Русский", callback_data="lang_ru"),
+            InlineKeyboardButton("🇬🇧 English", callback_data="lang_en"),
+        ],
+        [
+            InlineKeyboardButton("🇰🇬 Кыргызча", callback_data="lang_kg"),
+        ]
+    ]
+
+    return InlineKeyboardMarkup(keyboard)
