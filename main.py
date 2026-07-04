752311d2849567025912ab3403f2b93964aeba6e
loplol490-crypto <loplol490@gmail.com>
2026-07-04 19:45:33 +0600

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,10 @@ from telegram.ext import (
 )
 
 from handlers.start import start
-from keyboards.language_keyboard import language_keyboard
 from keyboards.menu import main_menu
 
 
-# обработка выбора языка
+# обработка выбора языка + переход в меню
 async def handle_language(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     await query.answer()
@@ -21,15 +20,15 @@ async def handle_language(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     if data == "lang_ru":
         context.user_data["lang"] = "ru"
-        await query.edit_message_text("Ты выбрал 🇷🇺 Русский")
+        await query.edit_message_text("Главное меню 👇", reply_markup=main_menu())
 
     elif data == "lang_en":
         context.user_data["lang"] = "en"
-        await query.edit_message_text("You selected 🇬🇧 English")
+        await query.edit_message_text("Main menu 👇", reply_markup=main_menu())
 
     elif data == "lang_kg":
         context.user_data["lang"] = "kg"
-        await query.edit_message_text("Сиз 🇰🇬 Кыргыз тилин тандадыңыз")
+        await query.edit_message_text("Башкы меню 👇", reply_markup=main_menu())
 
 
 # обработка меню
@@ -40,13 +39,13 @@ async def handle_menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
     data = query.data
 
     if data == "guide":
-        await query.edit_message_text("🗺 Гид по Кыргызстану")
+        await query.edit_message_text("🗺 Гид по Кыргызстану", reply_markup=main_menu())
     elif data == "food":
-        await query.edit_message_text("🍜 Еда Кыргызстана")
+        await query.edit_message_text("🍜 Еда Кыргызстана", reply_markup=main_menu())
     elif data == "transport":
-        await query.edit_message_text("🚌 Транспорт Кыргызстана")
+        await query.edit_message_text("🚌 Транспорт Кыргызстана", reply_markup=main_menu())
     elif data == "settings":
-        await query.edit_message_text("⚙️ Настройки")
+        await query.edit_message_text("⚙️ Настройки", reply_markup=main_menu())
 
 
 def main():
