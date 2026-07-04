fe0532210e353dd590edec5be07974950b7f86ef
loplol490-crypto <loplol490@gmail.com>
2026-07-04 19:38:50 +0600

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ from telegram.ext import (
 
 from handlers.start import start
 from keyboards.language_keyboard import language_keyboard
+from keyboards.menu import main_menu
 
 
-# обработка кнопок (с памятью языка)
+# обработка выбора языка
 async def handle_language(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     await query.answer()
@@ -31,11 +32,33 @@ async def handle_language(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await query.edit_message_text("Сиз 🇰🇬 Кыргыз тилин тандадыңыз")
 
 
+# обработка меню
+async def handle_menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+
+    data = query.data
+
+    if data == "guide":
+        await query.edit_message_text("🗺 Гид по Кыргызстану")
+    elif data == "food":
+        await query.edit_message_text("🍜 Еда Кыргызстана")
+    elif data == "transport":
+        await query.edit_message_text("🚌 Транспорт Кыргызстана")
+    elif data == "settings":
+        await query.edit_message_text("⚙️ Настройки")
+
+
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
+
+    # сначала язык
     app.add_handler(CallbackQueryHandler(handle_language))
+
+    # потом меню
+    app.add_handler(CallbackQueryHandler(handle_menu))
 
     app.run_polling()
 
