fd9810729271028d32d7a975f7d07abab978f1f7
loplol490-crypto <loplol490@gmail.com>
2026-07-05 18:48:08 +0600

    Update message.py

--- handlers/message.py
+++ handlers/message.py
@@ -1,12 +1,12 @@
 from telegram import Update
 from telegram.ext import ContextTypes
-
 from services.ai import get_ai_response
 
 
 async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_message = update.message.text
+    user_text = update.message.text
+    user_id = update.effective_user.id
 
-    response = await get_ai_response(user_message)
+    response = await get_ai_response(user_text, user_id)
 
     await update.message.reply_text(response)
